fd157c5eac09fbf72a347387e53b7249bf45f74c
AndyyR21 <andyyR167@gmail.com>
2026-01-21 20:30:46 -0800

    implement welcome section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import { Navbar, Welcome } from "#components"
+
 const App = () => {
   return (
-    <div>
-        <h1 className="text-3xl fond-bold underline">
-            Welcome to my MacFolio
-        </h1>
-    </div>
-  )
-}
+    <main>
+        <Navbar />
+        <Welcome />
+    </main>
+  );
+};
 
 export default App;
